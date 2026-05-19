c6b0556b5861be9e3166f8856f13542702ac26eb
Amelia Ortega Martinez <melyortegama@gmail.com>
2026-05-19 22:51:49 +0200

    first commit

---
+++ practica-ortega.php
@@ -0,0 +1,33 @@
+    <?php
+    /**
+     * Función que muestra un saludo personalizado en pantalla.
+     *
+     * @internal Esta función no devuelve nada, muestra directamente en pantalla
+     * @author Amelia Ortega Martínez
+     * @version 1.0
+     *
+     * @param string $nombre El nombre de la persona a saludar.
+     * @return void 
+     */
+    function saludar(string $nombre): void {
+        echo "¡Hola, " . $nombre . "!<br>";
+    }
+
+    /**
+     * Función que multiplica dos números.
+     *
+     * @internal Esta función es sólo para el núcleo del sistema, no usar en módulos
+     * @author Amelia Ortega Martínez
+     * @version 1.0
+     *
+     * @param int $a Primer número.
+     * @param int $b Segundo número.
+     * @return int El resultado de la multiplicación.
+     */
+    function multiplicar(int $a, int $b): int {
+        return $a * $b;
+    }
+
+    saludar("Juanjo"); 
+
+    echo "El resultado de la multiplicación es ". $resultado = multiplicar(5, 3);
